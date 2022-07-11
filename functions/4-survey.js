30135863046cbad0a6c778849df0a3a3acaeec75
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-11 12:00:48 -0400

    survey - check http methods

--- functions/4-survey.js
+++ functions/4-survey.js
@@ -6,22 +6,32 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
   .table('survey');
 
 exports.handler = async (event, context, cb) => {
-  try {
-    const { records } = await airtable.list();
-    // console.log(records);
-    const survey = records.map((item) => {
-      const { id } = item;
-      const { room, votes } = item.fields;
-      return { id, room, votes };
-    });
-    return {
-      statusCode: 200,
-      body: JSON.stringify(survey),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: 'Server Error',
-    };
+  const method = event.httpMethod;
+  if (method === 'GET') {
+    try {
+      const { records } = await airtable.list();
+      // console.log(records);
+      const survey = records.map((item) => {
+        const { id } = item;
+        const { room, votes } = item.fields;
+        return { id, room, votes };
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(survey),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: 'Server Error',
+      };
+    }
   }
+  if (method === 'PUT') {
+  }
+  // Default Response
+  return {
+    statusCode: 405,
+    body: 'Only GET and PUT Requests Allowed',
+  };
 };
