4f6348f2c161be4281ffecedc69d25a22962f159
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-11 17:25:16 -0400

    serverless function

--- functions/6-newsletter.js
+++ functions/6-newsletter.js
@@ -1,6 +1,36 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const url = 'https://api.buttondown.email/v1/subscribers';
+
 exports.handler = async (event, context, cb) => {
-  return {
-    statusCode: 400,
-    body: 'Our Newsletter Example',
-  };
+  const method = event.httpMethod;
+  if (method !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'ONLY POST Requests Allowed',
+    };
+  }
+  const { email } = JSON.parse(event.body);
+  try {
+    const data = await axios.post(
+      url,
+      { email },
+      {
+        headers: {
+          Authorization: `Token ${process.env.EMAIL_KEY}`,
+        },
+      }
+    );
+    console.log(data);
+    return {
+      statusCode: 201,
+      body: 'Success',
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error.response.data),
+    };
+  }
 };
