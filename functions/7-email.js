5892af5fdf0bd903933e36b5640d83d1d0a97bc9
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-11 18:24:24 -0400

    serverless structure

--- functions/7-email.js
+++ functions/7-email.js
@@ -1,4 +1,18 @@
 exports.handler = async (event, context, cb) => {
+  const method = event.httpMethod;
+  if (method !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Only POST Requests Allowed',
+    };
+  }
+  const { name, email, subject, message } = JSON.parse(event.body);
+  if (!name || !email || !subject || !message) {
+    return {
+      statusCode: 400,
+      body: 'Please Provide All Values',
+    };
+  }
   return {
     statusCode: 200,
     body: 'Our Email Example',
