8347456b70082270fb781b549874c379f4a03eec
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-11 13:08:30 -0400

    check for http methods

--- functions/5-weather.js
+++ functions/5-weather.js
@@ -1,8 +1,16 @@
 exports.handler = async (event, context, cb) => {
   const method = event.httpMethod;
-  console.log(method);
+
+  if (method !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Only POST Requests Allowed',
+    };
+  }
+  const { city } = JSON.parse(event.body);
+  console.log(city);
   return {
     statusCode: 200,
-    body: 'Our Weather Example',
+    body: JSON.stringify(city),
   };
 };
